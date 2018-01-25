89a90d2757a371c0d26914442ffdbf317627af6a
emanuel <emanuelbalcazar13@gmail.com>
2018-01-25 19:49:02 -0300

    Se agrego como dependencia externa openlayers v3

--- application/views/index.php
+++ application/views/index.php
@@ -8,7 +8,13 @@
 
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="images/favicon.png" />
+
+    <!-- Styles -->
     <link href="public/assets/app.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://openlayers.org/en/v3.16.0/css/ol.css" type="text/css">
+
+    <!-- Scripts -->
+    <script src="https://openlayers.org/en/v3.16.0/build/ol.js"></script>
     <script src="public/assets/app.min.js"></script>
     
 </head>
